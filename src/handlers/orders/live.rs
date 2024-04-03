9ea052f614973733c71de60e2787418a161d6422
clowzed <clowzed.work@gmail.com>
2024-04-03 23:07:25 +0300

    Added 10 orders on live ws connection

--- src/handlers/orders/live.rs
+++ src/handlers/orders/live.rs
@@ -6,7 +6,12 @@ use axum::{
     },
     response::Response,
 };
+use entity::{
+    order::{Column as OrderColumn, Entity as OrderEntity},
+    sea_orm_active_enums::Status,
+};
 use futures_util::{sink::SinkExt, stream::StreamExt};
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, QueryOrder, QuerySelect};
 use std::sync::Arc;
 use tokio::sync::mpsc;
 
@@ -21,10 +26,24 @@ async fn handle_socket(socket: WebSocket, state: Arc<AppState>) {
     let (tx, mut rx) = mpsc::channel(10);
     let (mut sender, _) = socket.split();
 
+    let orders_to_send = OrderEntity::find()
+        .filter(OrderColumn::Status.eq(Status::Succeeded))
+        .order_by_desc(OrderColumn::FinishedAt)
+        .limit(10)
+        .all(state.database_connection())
+        .await
+        .unwrap_or_default();
+
     tokio::spawn(async move {
         let connection = state.redis_client().get_async_connection().await.unwrap();
         let mut pubsub = connection.into_pubsub();
         pubsub.subscribe("live_orders").await.unwrap();
+
+        for order in orders_to_send {
+            tx.send(serde_json::to_string(&order).unwrap_or_default())
+                .await
+                .ok();
+        }
 
         while let Some(msg) = pubsub.on_message().next().await {
             if let Ok(payload) = msg.get_payload() {
